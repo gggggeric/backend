dea0b43688ff5642d2654d9cbc8ef9c38e3ef7eb
Geric <gericmorit3211@gmail.com>
2024-11-13 11:07:35 +0800

    preparing for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-u&cmv6up&=+_ugt&+t3t+rbwn*!62)a60ohdkwqsxa)n+x93@g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-backend.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['pythonProject-backend.onrender.com', 'localhost']
 
 
 
